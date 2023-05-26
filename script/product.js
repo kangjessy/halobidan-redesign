55196f3bbadc2752e60586d7e885833592135677
Jessy Mutaj Hifjudin <programmerjessy@gmail.com>
2023-05-26 09:07:02 +0700

    selesai

--- script/product.js
+++ script/product.js
@@ -1,20 +1,28 @@
 $(document).ready(function () {
-  $("#swipeProduct").slick({
-    slidesToShow: 3, // Jumlah card yang ditampilkan sekaligus
-    slidesToScroll: 1, // Jumlah card yang digeser saat navigasi
-    autoplay: true, // Otomatis memutar slider
-    autoplaySpeed: 2000, // Kecepatan autoplay dalam milidetik
+  var slider = $("#swipeProduct");
+
+  slider.slick({
+    slidesToShow: 4, // Jumlah kartu yang ditampilkan sekaligus
+    slidesToScroll: 1, // Pergeseran awal (tidak digunakan)
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: null,
+    nextArrow: null,
+    variableWidth: true,
+    swipe: true, // Aktifkan swipe
     responsive: [
       {
-        breakpoint: 768, // Ubah tampilan saat lebar layar kurang dari 768px
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
+          slidesPerRow: 2, // Jumlah kartu yang digeser saat lebar layar kurang dari 768px
         },
       },
       {
-        breakpoint: 480, // Ubah tampilan saat lebar layar kurang dari 480px
+        breakpoint: 480,
         settings: {
           slidesToShow: 1,
+          slidesPerRow: 1, // Jumlah kartu yang digeser saat lebar layar kurang dari 480px
         },
       },
     ],
